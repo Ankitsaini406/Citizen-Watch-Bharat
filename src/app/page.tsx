112a38b539cefa59c28c6f67878fe71e31f8fd69
Ankit Saini <as.ankitsaini406@gmail.com>
2025-07-01 15:38:14 +0530

    changes comma to variable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className="m-auto min-h-screen">
       <p className="text-base">
-        यह एक उदाहरण है कि कैसे आप 'द हिन्दू' जैसे शीर्षक और शरीर फॉन्ट्स का उपयोग कर सकते हैं।
+        यह एक उदाहरण है कि कैसे आप &apos;द हिन्दू&apos; जैसे शीर्षक और शरीर फॉन्ट्स का उपयोग कर सकते हैं।
       </p>
     </div>
   );
